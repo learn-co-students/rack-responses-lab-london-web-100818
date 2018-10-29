e5e4493569c5167d78e1df659ad12f58e233a3d7
congo-quinine <congo.quinine@gmail.com>
2018-10-29 11:02:22 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    time = Time.now
+
+      if time.hour < 12 && time.hour > 0
+        resp.write "Good Morning"
+      elsif time.hour > 12 && time.hour < 24
+        resp.write "Good Afternoon"
+      end
+  
+    resp.finish
+
+  end
+
+end
